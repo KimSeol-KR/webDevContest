cf56ac00048f746c702c7fadd0b851a98695db06
KimSeol-KR <kseol1017@gmail.com>
2025-01-18 01:23:03 +0900

    Add files via upload

--- JS/card.js
+++ JS/card.js
@@ -3,18 +3,20 @@ $.get('JSON/store.json').done((data) =>{
     $('#container').html('');
     
     data.products.forEach((a,i) => {
-        var card = `<div id="product-card" draggable="true">
+        var card = `<div class="product-card" draggable="true">
         <img src="img/${a.photo}" alt="상품이미지" draggable="false">
-        <div id="product-Info">
-        <h5 id="name">${a.title}</h5>
-        <p id="company">${a.brand}</p>
-        <h6><span id="price">${a.price}</span>원</h6>
+        <div class="product-Info">
+        <h5 class="name">${a.title}</h5>
+        <p class="company">${a.brand}</p>
+        <h6><span class="price">${a.price}</span>원</h6>
         </div>
-        <button id="add-Cart" class="btn btn-danger">담기</button>
+        <button class="btn btn-danger add-Cart">담기</button>
         </div>`
 
 
         $('#container').append(card);
 
     });
-})
+
+     
+});
--- JS/mouseControl.js
+++ JS/mouseControl.js
@@ -4,6 +4,45 @@
 // 2.5 요소 추가되면 hidden 클래스 추가해서 드래그 안내 없애기
 // 3. 이미 요소가 있다면 수량이 올라갈 것
 
-$("#product-card").mousedown((e) =>{
-    console.log(e);
-});
+// 드래그 이벤트 처리
+$(document).ready(function() {
+    let draggedItem = null;
+
+    // 드래그 시작: product-card를 드래그할 때
+    $('.product-card').on('dragstart', function(e) {
+      draggedItem = this; // 현재 드래그된 요소를 저장
+      $(this).css('opacity', 0.5); // 드래그 중 투명도를 낮춤
+    });
+
+    // 드래그 종료: 드래그가 끝났을 때
+    $('.product-card').on('dragend', function(e) {
+      $(this).css('opacity', 1); // 드래그 종료 후 원래 상태로 돌아옴
+    });
+
+    // 드래그 오버: #wish-List로 드래그된 항목이 들어갈 때
+    $('#wish-List').on('dragover', function(e) {
+      e.preventDefault(); // 기본 동작(드롭)을 허용하도록 설정
+      $(this).css('border', '2px dashed #00f'); // 드래그 중인 항목을 나타내기 위해 테두리 추가
+    });
+
+    // 드래그 종료: #wish-List에 항목을 드롭할 때
+    $('#wish-List').on('dragleave', function(e) {
+      $(this).css('border', 'none'); // 드래그 영역을 떠날 때 테두리 제거
+    });
+
+    // 항목을 드롭: #wish-List에 카드가 드롭되었을 때
+    $('#wish-List').on('drop', function(e) {
+      e.preventDefault(); // 기본 동작 방지
+      $('#drag-Zone').addClass('hidden');
+      $(this).css('border', 'none'); // 드래그 영역 테두리 제거
+
+        
+      $('#wish-List').addClass('wide');
+
+      // 드롭된 항목을 wish-List에 추가
+      let productCard = $(draggedItem).clone(); // 드래그된 카드를 복사해서 추가
+      $('#wish-List').append(productCard); // 복사된 카드를 wish-List에 추가
+    });
+  });
+
+
